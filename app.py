c29d485c3f9cc965942b32fbabb66e6c5512c7d4
Kirtan <105850020+Kirtan-Mudaliyar@users.noreply.github.com>
2025-06-19 00:05:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -69,11 +69,7 @@ def set_custom_theme():
 set_custom_theme()
 
 
-st.markdown("""
-    <h1 style='margin-bottom: 0.01rem;'>Fabric Defect Detection with YOLOv8</h1>
-    <hr style='margin-top: 0.01rem; border: 1px solid #444;' />
-""", unsafe_allow_html=True)
-
+st.title("Fabric Defect Detection with YOLOv8")
 st.markdown("The Fabric Defect Detector is a real-time AI-based web app built using YOLOv8 to identify fabric defects like holes, tears, and stains from images or live webcam input. Trained on the AITEX dataset and developed using Streamlit, it offers a fast and user-friendly solution for textile quality inspection.")
 
 # === 📌 Sidebar Info ===
